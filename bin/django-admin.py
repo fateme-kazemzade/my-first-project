e6478589381565fe2018b60490c530d652286e2b
Your Name <fateme.k128@gmail.com>
2016-07-24 16:23:19 +0430

    M y Django Girls app,first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/fateme/django-user/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.10rc1','django-admin.py'
+__requires__ = 'Django==1.10rc1'
+__import__('pkg_resources').run_script('Django==1.10rc1', 'django-admin.py')
